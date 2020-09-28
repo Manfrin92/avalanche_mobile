3e7c9c7caabf570a608612215bd7014ac2fb0195
Manfrin92 <manfredd_10@hotmail.com>
2020-09-28 09:02:47 -0300

    fix: teste

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import { Text, View } from 'react-native';
 
 const App: React.FC = () => {
     return (
-        <View>
-            <Text>Open up App.tsx to start working on your app!</Text>
+        <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
+            <Text> teste Open up App.tsx to start working on your app!</Text>
             <StatusBar backgroundColor="#F9F9F9" />
         </View>
     );
