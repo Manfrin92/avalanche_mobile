83fc3c08845f497113e97a5fe9f63a47cf60bca9
Manfrin92 <manfredd_10@hotmail.com>
2021-02-17 07:54:24 -0300

    feat: tratamento token vencido

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
+import { Alert, BackHandler } from 'react-native';
 import { APP_ENV } from '../../env';
 
 const api = axios.create({
@@ -18,5 +19,25 @@ api.interceptors.request.use(
     },
     (error: any) => Promise.reject(error),
 );
+
+api.interceptors?.response?.use(undefined, async (err: any) => {
+    const {
+        config,
+        response: { status },
+    } = err;
+
+    // // console.log(err);
+    if (status === 401) {
+        await AsyncStorage.multiRemove(['@Avalanche:token']);
+
+        Alert.alert('Faça seu login novamente.');
+
+        BackHandler.exitApp();
+
+        // DevSettings.reload();
+        return;
+    }
+    return Promise.reject(err);
+});
 
 export default api;
